d270fd9eb5f21918a88d767dd80a39fa55b69912
duydong03 <duydong03@gmail.com>
2015-02-20 10:41:19 -0600

    1st upload

---
+++ WebApplication8/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebApplication8.Startup))]
+namespace WebApplication8
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
