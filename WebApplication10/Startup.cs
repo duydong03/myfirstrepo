5d465ccaebfc7a4ee179dcb6eab6bd7c9bdb90e2
duydong03 <duydong03@gmail.com>
2015-02-26 11:52:24 -0600

    commit to vs_github branch

---
+++ WebApplication10/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebApplication10.Startup))]
+namespace WebApplication10
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
